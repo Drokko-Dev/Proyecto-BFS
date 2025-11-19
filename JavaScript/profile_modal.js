252c3579308228c8d7ed77941c47b9536938cf6b
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-11-19 14:59:37 -0300

    Profile Modal

---
+++ JavaScript/profile_modal.js
@@ -0,0 +1,9 @@
+
+
+const profile = document.querySelector("#profile");
+const menu = document.querySelector("#menu");
+profile.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    console.log(document.querySelector('#menu').className);
+})
+        
