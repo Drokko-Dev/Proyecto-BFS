eb554db551ec85b7791137f455cd768ad9124f8e
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-11-19 15:15:32 -0300

    corrected some details

--- JavaScript/profile_modal.js
+++ JavaScript/profile_modal.js
@@ -4,6 +4,5 @@ const profile = document.querySelector("#profile");
 const menu = document.querySelector("#menu");
 profile.addEventListener("click", () => {
     menu.classList.toggle("active");
-    console.log(document.querySelector('#menu').className);
 })
         
